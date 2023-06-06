18f9319b354d9d8c661a4a0e74620f7623696e3e
deedee <diddydeeable@gmail.com>
2023-06-06 04:55:34 +0100

    blah

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const io = new socketIO.Server(server);
 
 const path = require('path');
 
+const PAGE_SIZE = 10;
+
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'static', 'index.html');
   res.sendFile(filePath);
@@ -32,17 +34,44 @@ app.get('/client.js', (req, res) => {
 
   const axios = require('axios');
 
-  // Fetches and emits story data
-  const fetchAndEmitStories = async (socket) => {
-    try {
-        const storyIDs = (await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')).data;
-        const stories = await Promise.all(storyIDs.slice(0, 10).map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)));
-       //console.log('***Emitting stories:***', stories.map(story => story.data)); 
-        socket.emit('stories', stories.map(story => story.data));
-    } catch (error) {
-        console.error(`Failed to fetch stories: ${error}`);
-    }
+ 
+  
+// Fetches and emits story data
+const fetchAndEmitStories = async (socket,page = 1) => {
+  try {
+    const storyIDs = (await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')).data;
+    const start = (page - 1) * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+    const stories = await Promise.all(storyIDs.slice(start, end).map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)));
+
+    const orderedStories = stories.map(story => story.data).sort((a, b) => b.time - a.time);
+    socket.emit('stories', orderedStories);
+  } catch (error) {
+    console.error(`Failed to fetch stories: ${error}`);
+  }
 };
+
+  
+
+  const fetchAndEmitComments = async (socket, parentId) => {
+    try {
+      const response = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${parentId}.json`);
+      const commentIds = response.data.kids || [];
+      if (commentIds.length > 0) {
+        const commentPromises = commentIds.map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`));
+        const comments = await Promise.all(commentPromises);
+  
+        const orderedComments = comments.map(response => response.data).sort((a, b) => b.time - a.time);
+        socket.emit('comments', orderedComments)
+      } else {
+        console.log(`No comments for item with id ${parentId}`);
+        socket.emit('comments', []); // Emit an empty array if there are no comments
+      }
+    } catch (error) {
+      console.error(`Failed to fetch comments: ${error}`);
+    }
+  };
+
 
 const fetchAndEmitJobs = async (socket) => {
     try {
@@ -58,34 +87,41 @@ const fetchAndEmitJobs = async (socket) => {
     }
   };
   
-  const fetchAndEmitPolls = async (socket) => {
-    try {
-      const pollIDs = (await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')).data;
-      const pollPromises = pollIDs.slice(0, 10).map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`));
-      const pollResponses = await Promise.all(pollPromises);
-      const polls = pollResponses.map(response => response.data)
-        .filter(item => item.type === 'poll'); // Filter for polls only
-    
-      socket.emit('polls', polls); // Emit the polls to the client
-    
-      console.log('***Emitting polls:', polls);
-    } catch (error) {
-      console.error(`Failed to fetch polls: ${error}`);
-    }
-  };
+const fetchAndEmitPolls = async (socket) => {
+  try {
+    const pollIDs = [
+      31891675, 31869104, 31788898, 31780911, 31716715, 31598236, 31587976
+    ];
+    const polls = await Promise.all(pollIDs.map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)));
+    socket.emit('polls', polls.map(poll => poll.data));
+  } catch (error) {
+    console.error(`Failed to fetch polls: ${error}`);
+  }
+};
   
-  
-
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'none'; style-src 'self' 'unsafe-inline'; script-src 'self'; img-src 'self' data:; media-src 'self' data:;"
+  );
+  next();
+});
 
   io.on('connection', (socket) => {
     console.log('A user connected');
     fetchAndEmitStories(socket);
     fetchAndEmitJobs(socket);
     fetchAndEmitPolls(socket);
+   fetchAndEmitComments(socket);
   
     // event listener for stories
     socket.on('getStories', () => {
         fetchAndEmitStories(socket);
+    });
+
+    //event listener for stories
+    socket.on('getComments', (parentId) => {
+      fetchAndEmitComments(socket, parentId);
     });
 
     //event listner for jobs
@@ -102,6 +138,7 @@ const fetchAndEmitJobs = async (socket) => {
     socket.on('disconnect', () => {
       console.log('User disconnected');
     });
+
   });
 
 server.listen(3000, () => {
--- static/client.js
+++ static/client.js
@@ -1,85 +1,251 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const socket = io('http://localhost:3000');
-  
-    socket.on('connect_error', (error) => {
-      console.log(error);
+  const socket = io('http://localhost:3000');
+  let currentPage = 1; // Define currentPage as a global variable
+  socket.on('connect_error', (error) => {
+    console.log(error);
+
+  });
+
+  socket.on('connect', () => {
+    console.log('Connected to the server.');
+
+    socket.on('stories', (stories) => {
+      //console.log('Received stories:', stories);
+      renderPosts(stories);
+      paginate(stories, 10, currentPage); // 10 stories per page
     });
-  
-    socket.on('connect', () => {
-      console.log('Connected to the server.');
-  
-      socket.on('stories', (stories) => {
-        console.log('Received stories:', stories);
-        renderPosts(stories);
-      });
 
-
-      socket.on('jobs',(jobs) => {
-        console.log('Recieved jobs', jobs);
-        renderJobs(jobs);
-      })
-
-
-      socket.on('polls', (polls) => {
-        console.log('Received polls:', polls);
-        renderPolls(polls);
-      });
-
+    socket.on('comments',(comments) => {
+      console.log('Recieved comments***:', comments)
+      renderComments(comments);
     });
-  
+
+    socket.on('jobs', (jobs) => {
+     // console.log('Received jobs:', jobs);
+      renderJobs(jobs);
+    });
+
+    socket.on('polls', (polls) => {
+      console.log('Received polls:', polls);
+      renderPolls(polls);
+    });
+
     function renderPosts(stories) {
       const storyList = document.querySelector('#story-list');
       storyList.innerHTML = '';
-  
+
       for (let story of stories) {
         const storyElement = document.createElement('div');
         storyElement.textContent = story.title;
         storyElement.className = 'story-card';
+        socket.emit('getStories', currentPage);
+        // Make the story card clickable
+        storyElement.addEventListener('click', async () => {
+         // window.location.href = story.url; // Redirect to the story's URL
+          window.open(story.url, '_blank');
+          socket.emit('getComments', story.id);
+      //    console.log('recieved story:',story)
+        });
 
-      // Make the story card clickable
-      storyElement.addEventListener('click', () => {
-        window.location.href = story.url; // Redirect to the story's URL -> change so that a new web page is opened
-      });
+       
 
         storyList.appendChild(storyElement);
       }
     }
 
+// Step 1: Fetch the current maximum item ID
+async function fetchMaxItemId() {
+  try {
+    const response = await fetch('https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty');
+    if (response.ok) {
+      const maxItemId = await response.json();
+      console.log(maxItemId)
+      return maxItemId;
+    } else {
+      throw new Error('Failed to fetch the maximum item ID');
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// Step 2: Calculate the minimum item ID
+function calculateMinItemId(maxItemId) {
+  return maxItemId - 10; // Adjust the range as needed
+}
+
+// Step 3: Fetch the range of items within the specified range
+async function fetchItemsInRange(minItemId, maxItemId) {
+  try {
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${minItemId}..${maxItemId}.json`);
+    if (response.ok) {
+      const items = await response.json();
+      console.log(items)
+      return items;
+    } else {
+      throw new Error('Failed to fetch items within the range');
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// Step 4: Filter for comments
+function filterComments(items) {
+  return items.filter(item => item && item.type === 'comment');
+}
+
+// Step 5: Fetch and render comments
+async function fetchAndRenderComments() {
+  const maxItemId = await fetchMaxItemId();
+  if (!maxItemId) return;
+
+  const minItemId = calculateMinItemId(maxItemId);
+  const items = await fetchItemsInRange(minItemId, maxItemId);
+  if (!items) return;
+
+  const comments = filterComments(items);
+  renderComments(comments);
+}
+
+// Step 6: Render comments on the UI
+function renderComments(comments) {
+  // Render the comments on the UI as needed
+  console.log(comments);
+}
+// Step 6: Render comments on the UI
+function renderComments(comments) {
+  const commentList = document.querySelector('#comment-list');
+  commentList.innerHTML = ''; // Clear previous comments
+
+  for (const comment of comments) {
+    const commentElement = createCommentElement(comment);
+    commentList.appendChild(commentElement);
+  }
+
+  
+}
+
+    function createCommentElement(comment) {
+      const commentElement = document.createElement('div');
+      commentElement.textContent = comment.text;
+      commentElement.className = 'comment-card';
+      return commentElement;
+    }
+
+
 
     function renderJobs(jobs) {
-        const jobList = document.querySelector('#job-list');
-        jobList.innerHTML = '';
-    
-        for (let job of jobs) {
-          const jobElement = document.createElement('div');
-          jobElement.textContent = job.title;
-          jobElement.className = 'job-card';
+      const jobList = document.querySelector('#job-list');
+      jobList.innerHTML = '';
 
-        //make job cards clickable -> change so that a new web page is opened
+      for (let job of jobs) {
+        const jobElement = document.createElement('div');
+        jobElement.textContent = job.title;
+        jobElement.className = 'job-card';
+
+        // Make the job card clickable
         jobElement.addEventListener('click', () => {
-            window.location.href = job.url; // Redirect to the story's URL
-         });
-          jobList.appendChild(jobElement);
-        }
-      }
+          window.open(job.url, '_blank');
+        });
 
-      function renderPolls(polls) {
-        const pollList = document.querySelector('#poll-list');
-        pollList.innerHTML = '';
-      
-        for (let poll of polls) {
-          const pollElement = document.createElement('div');
-          pollElement.textContent = poll.title;
-          pollElement.className = 'poll-card';
-      
-          // Make the poll card clickable
-          pollElement.addEventListener('click', () => {
-            window.location.href = poll.url; // Redirect to the poll's URL
-          });
-      
-          pollList.appendChild(pollElement);
-        }
+        jobList.appendChild(jobElement);
       }
+    }
+
+    function renderPolls(polls) {
+      const pollList = document.querySelector('#poll-list');
+      pollList.innerHTML = '';
+
+      for (let poll of polls) {
+        const pollElement = document.createElement('div');
+        pollElement.textContent = poll.title;
+        pollElement.className = 'poll-card';
+
+        // Make the poll card clickable
+        pollElement.addEventListener('click', () => {
+          // Handle the click event and perform the desired action
+          openPoll(poll);
+        });
+
+        pollList.appendChild(pollElement);
+      }
+    }
+
+    function openPoll(poll) {
+      // Check if the poll has a URL
+      if (poll.url) {
+        window.open(poll.url, '_blank'); // Open the poll's URL in a new tab
+      } else {
+        // If the poll doesn't have a URL, fetch the poll data to retrieve the URL
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${poll.id}.json?print=pretty`)
+          .then(response => response.json())
+          .then(pollData => {
+            if (pollData.url) {
+              window.open(pollData.url, '_blank'); // Open the retrieved URL in a new tab
+            } else {
+              console.log(`Poll with ID ${poll.id} doesn't have a URL`);
+            }
+          })
+          .catch(error => {
+            console.error(`Failed to fetch poll data: ${error}`);
+          });
+      }
+    }
+    
+
+
+    function paginate(data, limit, currentPage = 1) {
+      const pagination = document.querySelector("#pagination");
+      pagination.textContent = "";
+      
+      const pageCount = Math.ceil(data.length / limit);
+      const start = (currentPage - 1) * limit;
+      const end = start + limit;
+      const pageData = data.slice(start, end);
+    
+      // Add the 'Previous' button
+      const prevButton = document.createElement("button");
+      prevButton.textContent = "Previous";
+      prevButton.onclick = () => {
+        if(currentPage > 1) {
+          currentPage--;
+          socket.emit('getStories', currentPage); // Emit the getStories event with the new page number
+          paginate(data, limit, currentPage);
+        }
+      };
+      pagination.append(prevButton);
+      
+      // Numbered buttons for each page
+      for (let i = 1; i <= pageCount; i++) {
+        const button = document.createElement("button");
+        button.textContent = i;
+        button.onclick = () => {
+          currentPage = i;
+          socket.emit('getStories', currentPage); // Emit the getStories event with the new page number
+          paginate(data, limit, i);
+        };
+        pagination.append(button);
+      }
+      
+      // Add the 'Next' button
+      const nextButton = document.createElement("button");
+      nextButton.textContent = "Next";
+      nextButton.onclick = () => {
+        if(currentPage < pageCount) {
+          currentPage++;
+          socket.emit('getStories', currentPage); // Emit the getStories event with the new page number
+          paginate(data, limit, currentPage);
+        }
+      };
+      pagination.append(nextButton);
+      
+      renderPosts(pageData); // render the posts for this page
+    }
+    
+
+
   });
-  
- 
+});
